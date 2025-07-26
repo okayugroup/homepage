e5abb672e53bc6213add29c52b1728709bafe6da
yossy4411 <yossy4411.dev@gmail.com>
2025-07-26 10:55:28 +0900

    Adminページに新しいセクションを追加し、レイアウトを調整

--- src/app/team/administrators/page.tsx
+++ src/app/team/administrators/page.tsx
@@ -11,12 +11,38 @@ export const metadata: Metadata = {
 export default function AdminPage() {
     return <AdminBody>
         <Header currentPath="/team/administrators" pane="admin" />
-        <main className="mt-8 px-20">
-            <section className="py-30">
+        <main className="mt-18 py-30 px-60 space-y-40">
+            <section>
                 <h1 className="text-3xl font-bold">お知らせ</h1>
-                <ul className="list-inside gap-6 mt-4 ml-8">
-                    <li>おかゆグループ Administratorsの作成を開始<span className="text-sm">（2025/07/25更新）</span></li>
+                <ul className="list-inside gap-6 mt-8 ml-4">
+                    <li>おかゆグループ Administratorsを立ち上げました<span className="text-sm">（2025/07/26更新）</span></li>
                 </ul>
+            </section>
+            <section>
+                <h1 className="text-3xl font-bold">ブログ</h1>
+                <div className="grid grid-cols-1 gap-6 mt-8">
+                    ここには（まだ）何もありません。
+                </div>
+            </section>
+            <section>
+                <h1 className="text-3xl font-bold">プロジェクト</h1>
+                <div className="grid grid-cols-1 gap-6 mt-8">
+                    ここには（まだ）何もありません。（めんどくさいから書いてないだけ）
+                </div>
+            </section>
+            <section>
+                <h1 className="text-3xl font-bold">動作状況</h1>
+                <div className="grid grid-cols-1 xl:grid-cols-2 gap-6 mt-8">
+                    <div className="flex flex-row gap-2 bg-gray-100 dark:bg-gray-800 p-2 rounded-xl border-2 border-green-500">
+                        <div className="p-3">
+                            <h2 className="text-xl font-semibold">サーバー</h2>
+                            <p className="text-gray-600 dark:text-gray-400 mt-2">正常に動作しています。</p>
+                        </div>
+                        <div className="flex items-center bg-white/80 rounded-lg flex-1 p-4">
+                            <span>ここにグラフ入れたい🤔</span>
+                        </div>
+                    </div>
+                </div>
             </section>
         </main>
     </AdminBody>;
