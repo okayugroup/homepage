fad95102c5b5e54bb3dec449fb8f4d0fddd02a65
yossy4411 <yossy4411.dev@gmail.com>
2025-07-29 21:53:22 +0900

    '/'にて、ダークモードでのヘッダーの透明度を下げてブラーを見せる

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,7 +26,7 @@ export function Header({ currentPath, pane }: { currentPath: string, pane?: "adm
                 {path: "/contact", label: "お問い合わせ"},
             ]
             return (
-                <header className="flex items-center justify-between p-2 bg-gray-100/75 dark:bg-gray-800 fixed top-0 left-0 right-0 z-50 backdrop-blur-sm border-b border-b-gray-200 dark:border-b-gray-700">
+                <header className="flex items-center justify-between p-2 bg-gray-100/75 dark:bg-gray-800/60 fixed top-0 left-0 right-0 z-50 backdrop-blur-sm border-b border-b-gray-200 dark:border-b-gray-700">
                     <Image src="/okayugroup.svg" alt="おかゆグループ" width={200} height={50} className="relative h-12" />
                     <nav className="my-2">
                         <ul className="flex space-x-4">
