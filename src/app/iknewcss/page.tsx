6a816147077bb5cba356dd746613b1d9e7742279
yossy4411 <yossy4411.dev@gmail.com>
2025-08-03 09:57:49 +0900

    CSSを完全に理解した人のページを追加

---
+++ src/app/iknewcss/page.tsx
@@ -0,0 +1,9 @@
+export default function IKnewCSSPage(): JSX.Element {
+    return (
+        <div className="flex justify-center items-center w-screen h-screen">
+            <div className="w-32 h-18 border-2 text-nowrap p-5 text-lg font-semibold text-red-500 border-black">
+                CSS完全に理解した
+            </div>
+        </div>
+    );
+}
