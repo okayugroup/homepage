1182598e20756ca684b539602389bca50be95da4
yossy4411 <yossy4411.dev@gmail.com>
2025-08-05 15:29:10 +0900

    エラーを破壊

--- src/app/iknewcss/page.tsx
+++ src/app/iknewcss/page.tsx
@@ -1,3 +1,5 @@
+import {JSX} from "react";
+
 export default function IKnewCSSPage(): JSX.Element {
     return (
         <div className="flex justify-center items-center w-screen h-screen">
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import {JSX} from "react";
+
 export function Footer(): JSX.Element {
     return (
         <footer className="bg-gray-100 dark:bg-gray-800 text-gray-600 dark:text-gray-300 p-4 py-8 text-center">
