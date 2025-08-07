84fa19d134b209ec26379892e726237d8aefa397
yossy4411 <yossy4411.dev@gmail.com>
2025-08-07 18:55:52 +0900

    誕生日を書こう

--- src/db/members.tsx
+++ src/db/members.tsx
@@ -1,6 +1,7 @@
 import {JSX} from "react";
 import Image from "next/image";
 import {Roboto_Mono} from "next/font/google";
+import {LiaBirthdayCakeSolid} from "react-icons/lia";
 
 export type MemberData = {
     name: string;
@@ -9,7 +10,11 @@ export type MemberData = {
         [key: string]: string;
     };
     languages?: { [lang: string]: number };
-    additionalTags?: { [tag: string]: string | number | boolean;}
+    additionalTags?: {
+        birthday?: Date;
+        sex?: "male" | "female" | "other";
+        location?: string;
+    }
 }
 
 const codeFont = Roboto_Mono({
@@ -27,15 +32,46 @@ export class Member {
     }
 
     getHtml(): JSX.Element {
-        const properies = this.data.additionalTags ? Object.entries(this.data.additionalTags).map(([key, value]) => (`${key}: ${value}`)).join("\n") : "";
+        const properties: string[] = [this.id];
+        switch (this.data.additionalTags?.sex) {
+            case "male":
+                properties.push("男");
+                break;
+            case "female":
+                properties.push("女");
+                break;
+            case "other":
+                properties.push("その他");
+                break;
+        }
         return (
             <div className="flex flex-row gap-6 px-60">
                 <div className="w-1/3 max-w-120 p-1">
                     <Image alt={`${this.data.name}のアイコン`} src={`/members/${this.id}.webp`} width={256} height={256} className="rounded-full border border-gray-300 mb-4" />
                     <section>
                         <h2 className="text-2xl font-bold mb-1">{this.data.name}</h2>
-                        <p className={`text-sm mb-3`}><span className={codeFont.className}>{this.id}</span> · 男 · 2010年生</p>
-                        <p className="text-gray-500">{this.data.description}</p>
+                        <div className="text-sm mb-3 text-gray-700">
+                            <p>
+                                {(()=> {
+                                    const inner = properties.join(" · ")
+                                    if (this.data.additionalTags?.birthday) {
+                                        return <>
+                                            <span>{inner + " · "}</span>
+                                            <span className="relative group">
+                                                {this.data.additionalTags?.birthday!.getFullYear()}年生
+                                                <span className="text-gray-800 absolute opacity-0 group-hover:opacity-100 backdrop-blur-xs left-1 top-1 p-2 max-w-max rounded-xl shadow border border-gray-100 transition-opacity duration-150 bg-white/20">
+                                                    <LiaBirthdayCakeSolid />{this.data.additionalTags.birthday.toLocaleDateString("ja-JP")}
+                                                </span>
+                                            </span>
+                                        </>
+
+                                    }
+                                    return inner;
+                                })()}
+                            </p>
+                        </div>
+
+                        <p className="">{this.data.description}</p>
                     </section>
 
                 </div>
@@ -85,9 +121,9 @@ export const Members: { [key: string]: Member } = {
 
             },
             additionalTags: {
-                "role": "member",
-                "age": 14,
-                "location": "Shiga, Japan",
+                birthday: new Date("2010-08-16"),
+                location: "Shiga, Japan",
+                sex: "male",
             }
         }
     )
