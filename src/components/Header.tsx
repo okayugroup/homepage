11a880cbd57c942ab0d8e274f18badaf5c8f8ccf
yossy4411 <yossy4411.dev@gmail.com>
2025-08-19 13:02:27 +0900

    ヘッダーのナビゲーションのパディングを修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ export function Header({ currentPath, pane }: { currentPath: string, pane?: stri
                 <header className="flex items-center justify-between p-2 bg-gray-100/75 dark:bg-gray-800/60 fixed top-0 left-0 right-0 z-50 backdrop-blur-sm border-b border-b-gray-200 dark:border-b-gray-700">
                     <Image src="/okayugroup.svg" alt="おかゆグループ" width={200} height={50} className="relative h-12" />
                     <nav className="my-2">
-                        <ul className="flex space-x-4">
+                        <ul className="flex space-x-4 px-3">
                             {
                                 urls.map((url) => (
                                     <li key={url.path}>
